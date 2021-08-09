3d9339cc54aa28a213e4875bf943d7113948844e
Aliaksandr <sasha.tarasiuk90@gmail.com>
2021-08-09 22:32:54 +0300

    HW1: Aliaksandr Tarasiuk

--- src/task_1_2.py
+++ src/task_1_2.py
@@ -1,7 +1,4 @@
-# Task №2
-x = int(input('Введите значение "х:"'))
-y = int(input('Введите значение "y:"'))
-
-e = (abs(x)-abs(y))/(1+abs(x * y))
-
-print(e)
+x,y = int(input()), int(input())
+c = x - y
+d = (x * y) + 1
+print (c/d)
