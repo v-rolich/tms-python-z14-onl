09b8512dd908acca23f88f5d41ee1a09710f54b2
Aliaksandr <sasha.tarasiuk90@gmail.com>
2021-08-09 21:13:10 +0300

    HW1: Aliaksandr Tarasiuk

---
+++ src/task_1_1.py
@@ -0,0 +1,10 @@
+a,b = int(input("Введите первое число:")), int(input("Введите второе число:"))
+c = str(input("Введите символ"))
+if c == "-":
+    print(a-b)
+elif c == "+":
+    print(a+b)
+elif c == "*":
+    print (a*b)
+else:
+    print ("Данные введены некоректно")
---
+++ src/task_1_2.py
@@ -0,0 +1,4 @@
+x,y = int(input()), int(input())
+c = x - y
+d = (x * y) + 1
+print (c/d)
