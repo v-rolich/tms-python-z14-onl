df0ca819f7f8426a7fde190826c1e92fcf09a191
Vladimir-Surta <surtusina@gmail.com>
2021-08-09 18:50:43 +0300

    "HW3:Vladimir_Surta_4"

---
+++ src/task_1_1.py
@@ -0,0 +1,9 @@
+# Task №1
+a = 5
+b = 7
+
+c = a + b
+d = a - b
+
+print( {c,d})
+print(a * b)
---
+++ src/task_1_2.py
@@ -0,0 +1,7 @@
+# Task №2
+x = int(input('Введите значение "х:"'))
+y = int(input('Введите значение "y:"'))
+
+e = (abs(x)-abs(y))/(1+abs(x * y))
+
+print(e)
