7366a5a29963199fab5f80368cfa2c5ba3fdb1c7
BelovVladimirO <83946250+BelovVladimirO@users.noreply.github.com>
2021-08-09 11:45:16 +0300

    Add files via upload

---
+++ src/task_1_1.py
@@ -0,0 +1,6 @@
+a = 7
+b = 12
+c = a + b
+d = a - b
+f = a * b
+print(c, d, f)
---
+++ src/task_1_5.py
@@ -0,0 +1,7 @@
+import math
+a = 8
+b = 15
+d = (a * a) + (b * b)
+sqrt = math.sqrt(d)
+s = a * b // 2
+print(int(sqrt), s)
