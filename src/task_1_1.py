31b7e8c00e3bcf0937e339237b3c380e67ab24af
sergey <elhom99@gmail.com>
2021-08-09 16:55:33 +0300

    HW1: Sergey Selivonchik

---
+++ src/task_1_1.py
@@ -0,0 +1,8 @@
+#Даны 2 действительных числа a и b. Получить их сумму, разность и произведение.
+a=10
+b=-5
+summ=a+b
+diff=a-b
+mult=a*b
+print ("a="+str(a)+"\nb="+str(b)+
+       "\na+b="+str(summ)+ "\na-b="+str(diff)+ "\na*b="+str(mult))
