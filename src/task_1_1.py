192ece40c2b18dbaff59d05524ca12926e013ff9
Uladzimir Rolich <45147277+v-rolich@users.noreply.github.com>
2021-08-10 11:00:55 +0300

    Delete src directory

--- src/task_1_1.py
+++
@@ -1,9 +0,0 @@
-# Task №1
-a = 5
-b = 7
-
-c = a + b
-d = a - b
-
-print( {c,d})
-print(a * b)
--- src/task_1_2.py
+++
@@ -1,7 +0,0 @@
-# Task №2
-x = int(input('Введите значение "х:"'))
-y = int(input('Введите значение "y:"'))
-
-e = (abs(x)-abs(y))/(1+abs(x * y))
-
-print(e)
