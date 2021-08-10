8992b937e6161cf98439560a3e62f5dcc82d4fc0
sergey <elhom99@gmail.com>
2021-08-10 21:13:29 +0300

    HW1: Sergey Selivonchick

---
+++ src/task_1_1.py
@@ -0,0 +1,8 @@
+# Даны 2 действительных числа a и b. Получить их сумму, разность и произведение.
+a = 10
+b = -5
+s = a + b
+diff = a - b
+m = a * b
+print(f"a= {a} \nb= {b} \na+b= {s} \na-b= {diff} \na*b= {m}")
+# print("a="+str(a)+"\nb="+str(b) + "\na+b="+str(s) + "\na-b="+str(diff) + "\na*b="+str(m))
---
+++ src/task_3_1.py
@@ -0,0 +1,7 @@
+# Создать переменные firstname, lastname, age с соответствующими значениями
+# Путем сложения получить строку вида Привет, меня зовут Иван Иванов, мне 35 лет
+# Примечание:Переменную age задавать как строку ‘35’
+firstname = input("Input your name: ")
+lastname = input("Input your surname: ")
+age = input("Input your age: ")
+print("Привет, меня зовут " + firstname + " " + lastname + " мне " + age + " лет")
