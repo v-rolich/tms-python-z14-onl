9b745416bccf929dd8017f3a24e2027acd822a9a
Grigoash Pasha <GrigorashPasha@gmail.com>
2021-08-11 13:42:37 +0300

    HW1: Grigorash Pavel

--- src/task_3_3.py
+++ src/task_3_3.py
@@ -10,7 +10,7 @@ We will get an error in the second case (second_dict)
 In python dictionary keys must be immutable types (if key can change, there will be problems) and list is a mutable type
 """
 films = ['Lord of the Rings', 'Lord of the Ring: Two tower']
-actors = ("Gendalf", "Frodo")
+actors = ("Geendalf", "Frodo")
 first_dict = {actors: films}
 second_dict = {films: actors}
 print(first_dict)
