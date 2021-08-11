897af3982ff0e7639ff629f9adb0f96673e055de
Vitaly-GR <vsparazzi@gmail.com>
2021-08-12 01:30:05 +0300

    fixed mistakes in tasks 1.1-3.4

--- src/task1_2.py
+++ src/task1_2.py
@@ -1,10 +1,9 @@
+# Даны действительные числа x и y. Получить |x|-|y|/(1+|xy|)
 def func_2(a, b):
-    c = int(a)
-    d = int(b)
-    print((abs(c) - abs(d)) / (1 + abs(c * d)))
+
+    print((abs(a) - abs(b)) / (1 + abs(a * b)))
 
 
 x = int(input('x='))
 y = int(input('y='))
 func_2(x, y)
-# Даны действительные числа x и y. Получить |x|-|y|/(1+|xy|)
--- src/task3_2.py
+++ src/task3_2.py
@@ -1,24 +1,16 @@
-list_1 = list('numbers')
-list_2 = list('letters')
-list_1.append(1)
-list_2.append('z')
-
-
-def func_1(a, b):
-    print(a)
-    print(b)
-
-
-func_1(list_1, list_2)
-
-list_1.insert(0, 10)
-list_2.insert(0, 'B')
-
-func_1(list_1, list_2)
-
-list_1.extend(list_2)
-print(list_1)
 # Создать два списка произвольного содержания.
 # Добавить к каждому списку по одному элементу в конец и в начало.
 # Расширить первый список вторым.
 # Все изменения выводить на экран.
+list_1 = list('numbers')
+list_2 = list('letters')
+list_1.append(1)
+list_2.append('z')
+print(list_1)
+print(list_2)
+list_1.insert(0, 10)
+list_2.insert(0, 'B')
+print(list_1)
+print(list_2)
+list_1.extend(list_2)
+print(list_1)
