41e4bab403eaa6cc6a5587ad73ea95382fa39a78
sergey <elhom99@gmail.com>
2021-08-12 17:50:51 +0300

    sss

---
+++ src/task_3_6.py
@@ -0,0 +1,10 @@
+# Запросить у пользователя возраст. Если возраст меньше
+# 0 - вывести Wrong input, если меньше 18 - вывести
+# CocaCola, иначе - вывести Beer
+age = int(input("Введите свой возраст:\n"))
+if age < 0:
+    print("Wrong input")
+elif age < 18:
+    print("CocaCola")
+else:
+    print("Beer")
