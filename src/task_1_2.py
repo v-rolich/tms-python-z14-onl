f9f8cd9d2e630be7dae21a1b8c5ac9f9f49f9691
viktoria Yushkevich <victoshik095@gmail.com>
2021-08-14 13:04:01 +0300

    HW1: Yushkevich Victoria

--- src/task_1_2.py
+++ src/task_1_2.py
@@ -1,5 +1,6 @@
+# Даны действительные числа x и y. Получить 1+ |xy|/|x| − |y|
+
 x = float(input("Enter x"))
 y = float(input("Enter y"))
 z = (abs(x) - abs(y)) / (1 + abs(x * y))
 print(z)
-# Даны действительные числа x и y. Получить 1+ |xy|/|x| − |y|
