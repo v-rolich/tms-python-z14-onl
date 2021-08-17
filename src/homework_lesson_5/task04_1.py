17dd9de46d2c0f827270c628bf887cc2d436150c
Vitaly-GR <88375984+Vitaly-GR@users.noreply.github.com>
2021-08-17 23:04:28 +0300

    Update task04_1.py

--- src/homework_lesson_5/task04_1.py
+++ src/homework_lesson_5/task04_1.py
@@ -1,14 +1,14 @@
 # 1) Дан список целых чисел.
 # Создать новый список, каждый элемент которого равен исходному элементу
 # умноженному на -2
-list_1 = [12, 15, 14, 45, 2, 6, 9]
-list_2 = [list_2 * (-2) for list_2 in list_1]
-print(list_2)
+f_lst = [12, 15, 14, 45, 2, 6, 9]
+s_lst = [s_lst * (-2) for s_lst in f_lst]
+print(s_lst)
 
-a = len(list_1)
+a = len(f_lst)
 c = []
 d = 0
 while d < a:
-    c.append(list_1[d] * (-2))
+    c.append(f_lst[d] * (-2))
     d += 1
 print(c)
