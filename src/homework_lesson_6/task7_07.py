54bf293f319d64a5075c4bfa01c10476bc636d19
Vitaly-GR <88375984+Vitaly-GR@users.noreply.github.com>
2021-08-20 00:25:09 +0300

    Update task7_07.py

--- src/homework_lesson_6/task7_07.py
+++ src/homework_lesson_6/task7_07.py
@@ -3,12 +3,9 @@
 # аргументов и выводит на экран те из них,
 # длина ключа которых четна.
 def even_args(**kwargs):
-    k = []
-    for i in kwargs:
-        if len(i) % 2 == 0:
-            k.append(i)
-    a = f'Even keys :\n {k}'
-    return a
+    for k, v in kwargs.items():
+        if len(k) % 2 == 0:
+            print(k, v)
 
 
-print(even_args(map=7, home=8, line=6, function=14))
+even_args(map=7, home=8, line=6, function=14)
