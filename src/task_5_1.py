ab16d5b4b57e2135e81eeab399e517ef20c8d968
Polina <polinalashkevich72@gmail.com>
2021-08-19 09:17:45 +0300

    HW5: Polina Lashkevich

---
+++ src/task_5_1.py
@@ -0,0 +1,16 @@
+#Дан список целых чисел.
+#Создать новый список, каждый элемент которого равен исходному элементу
+#умноженному на -2
+
+first_list = [1, 2, 3, 4, 5]
+second_list = [second_list * (-2) for second_list in first_list]
+
+print(second_list)
+
+lenght = len(first_list)
+third_list = []
+i = 0
+while i < lenght:
+    third_list.append(first_list[i] * (-2))
+    i += 1
+print(third_list)
---
+++ src/task_5_2.py
@@ -0,0 +1,18 @@
+#Дан список целых чисел. Подсчитать сколько четных чисел в списке
+
+
+first_list = list(range(1, 50))
+count = 0
+for i in first_list:
+    if i % 2 == 0:
+        count += 1
+print(count)
+
+lenght = len(first_list)
+total = 0
+second_count = 0
+while total < lenght:
+    if first_list[total] % 2 == 0:
+        second_count += 1
+    total += 1
+print(second_count)
