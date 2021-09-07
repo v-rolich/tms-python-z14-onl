9c8f48944a8b36f531c34883459d1841c3778239
Grigoash Pasha <GrigorashPasha@gmail.com>
2021-09-07 14:07:32 +0300

    HW11: Grigorash Pavel

--- src/task_14_4.py
+++ src/task_14_4.py
@@ -4,15 +4,23 @@
 """
 
 import argparse as ar
+import csv
 
 parser = ar.ArgumentParser()
 parser.add_argument('-fn', '--first-name',
                     required=True)
 parser.add_argument('-ln', '--last-name',
                     required=True)
-parser.add_argument('-a', '--age', required=True)
+parser.add_argument('-a', '--age',
+                    required=True)
 args = parser.parse_args()
 print(args)
 
+lst = [args.first_name, args.last_name, args.age]
 with open('File.csv', 'w') as csv_file:
-    csv_file.writelines(f'{args}')
+    write = csv.writer(csv_file)
+    write.writerow(lst)
+
+print('First name:', args.first_name)
+print('Last name:', args.last_name)
+print('Age:', args.age)
