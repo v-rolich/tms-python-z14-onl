b70a29b11a6124d87dd72bb9e3e06048c36c9538
Grigoash Pasha <GrigorashPasha@gmail.com>
2021-09-09 10:19:47 +0300

    HW12: Grigorash Pavel

--- src/Connect_db.py
+++ src/Connect_db.py
@@ -8,4 +8,3 @@ for row in cur.execute('SELECT * FROM Department d join Course c on d.id = c.dep
                        'join Teacher t on d.id = t.department'
                        ' Where c.title=:name', {'name': 'Python'}):
     print(row)
-
