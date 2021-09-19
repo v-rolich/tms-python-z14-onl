84410cd0f6a49fd8fe8544cf6dc014554fc6cf45
Grigorash Pavel <GrigorashPasha@gmail.com>
2021-09-19 13:39:38 +0300

    HW15: Grgigorash Pavel

--- src/task_1.py
+++ src/task_1.py
@@ -1,9 +1,23 @@
+"""
+Создать 3 массива (использовать numpy), содержащих по 11 элементов:
+1 - содержит имена;
+2 - содержит возраст (от 18 до 40); сгенерировать случаным образом;
+3 - содержит рост (от 160 до 2050); сгенерировать случаным образом;
+На основе массивов создать Data Frame.
+Отсортировать данные в фрейме по возрасту.
+Увеличить возраст на 5 лет (для каждой записи).
+Уменьшить рост на 1%.
+Вывести на экран первае 5 записей во фрейме.
+Данные записить в csv файл.
+"""
+
 import numpy as np
 import pandas as pd
 
 
-name = np.array(['Vladimir', 'Pavel', 'Vitaliy', 'Polina', 'Alexei', 'Tom'
-                 'Jack', 'Shon', 'Marta', 'Valeria', 'Kate', 'Mikle'])
+name = np.array(['Vladimir', 'Pavel', 'Vitaliy', 'Polina', 'Alexei', 'Tom',
+                 'Jack', 'Shon', 'Marta', 'Valeria', 'Kate']
+                )
 age = np.random.randint(18, 40, (1, 11))
 height = np.random.randint(160, 2050, (1, 11))
 
